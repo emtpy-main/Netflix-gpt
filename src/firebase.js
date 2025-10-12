3e64d6888364f42b0ede5ae94ed70083fba9f829
3pratik2 <singhpratik2210@gmail.com>
2025-10-12 22:55:53 +0530

    index.html updated

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,10 @@
-// Import the functions you need from the SDKs you need
+ 
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ 
 const firebaseConfig = {
   apiKey: "AIzaSyAYfMxPqi5s_nO6mT1PwVxb0wAla4ZReqU",
   authDomain: "netflixgpt-fe343.firebaseapp.com",
@@ -16,9 +13,7 @@ const firebaseConfig = {
   messagingSenderId: "59733765448",
   appId: "1:59733765448:web:bff3b872efeb3d075e577d",
   measurementId: "G-21EXNDYKJN"
-};
-
-// Initialize Firebase
+}; 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth = getAuth();
